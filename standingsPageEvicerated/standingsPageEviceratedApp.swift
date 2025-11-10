5bf860b2ee79252ae6bdc4fd0c331e86fbd9507f
Theadora Yocum <theayocum@mac.com>
2025-11-10 11:25:34 -0500

    Initial Commit

---
+++ standingsPageEvicerated/standingsPageEviceratedApp.swift
@@ -0,0 +1,17 @@
+//
+//  standingsPageEviceratedApp.swift
+//  standingsPageEvicerated
+//
+//  Created by Thea Yocum on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct standingsPageEviceratedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
